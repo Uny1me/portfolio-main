8280b3b98a8ce4f0aea88e1cb7b2928ff3267bef
Ekim Unyime <Uny1me@users.noreply.github.com>
2020-12-22 03:21:22 +0100

    commit lock

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '5e2469e388210d454ffc2f8ec2a5bbc4813dcaa0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5e2469e388210d454ffc2f8ec2a5bbc4813dcaa0',
+    ),
+  ),
+);
